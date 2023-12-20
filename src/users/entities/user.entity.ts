422ae0a7457094a054d7b6160d04fffe5bc6e70f
JongminChoi98 <joeychoi7624@gmail.com>
2023-12-20 14:06:09 -0800

    [BUG FIXED] kept hash password when update user profile

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,5 @@
 import { CoreEntity } from 'src/common/entities/core.entity';
-import * as bcrypt from 'bcrypt';
-import { Entity, Column, BeforeInsert, BeforeUpdate } from 'typeorm';
-import { InternalServerErrorException } from '@nestjs/common';
+import { Entity, Column } from 'typeorm';
 
 @Entity()
 export class User extends CoreEntity {
@@ -22,24 +20,4 @@ export class User extends CoreEntity {
 
   @Column('boolean', { default: 0 })
   hasAddress: boolean;
-
-  @BeforeInsert()
-  @BeforeUpdate()
-  async hashPassword(): Promise<void> {
-    if (this.password) {
-      try {
-        this.password = await bcrypt.hash(this.password, 10);
-      } catch (error) {
-        throw new InternalServerErrorException();
-      }
-    }
-  }
-
-  async checkPassword(aPassword: string): Promise<boolean> {
-    try {
-      return await bcrypt.compare(aPassword, this.password);
-    } catch (error) {
-      throw new InternalServerErrorException();
-    }
-  }
 }
